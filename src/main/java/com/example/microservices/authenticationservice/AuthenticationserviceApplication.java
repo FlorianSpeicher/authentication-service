477e9650d682bf79dcd7d421de4b9157794206eb
Florian Speicher <florian.speicher@abatplus.de>
2024-03-08 09:07:51 +0100

    Finished project on Docker!!!

--- src/main/java/com/example/microservices/authenticationservice/AuthenticationserviceApplication.java
+++ src/main/java/com/example/microservices/authenticationservice/AuthenticationserviceApplication.java
@@ -2,6 +2,7 @@ package com.example.microservices.authenticationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
 public class AuthenticationserviceApplication {
